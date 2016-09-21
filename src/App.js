f0103e74b506a76551f5f5d8fc95b327f003be4d
andosteinmetz <ando.steinmetz@gmail.com>
2016-09-21 19:08:12 -0400

    setState not working from within handleChange

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Person from './Person';
 import Form from './Form';
 
+var update = require('react-addons-update');
+
 const state = {
   editing: false,
   customer: {
@@ -9,6 +11,12 @@ const state = {
     lastName: 'Irwin',
     company: 'Artforum',
     title: 'Founder'
+  },
+  newCustomer: {
+    firstName: '',
+    lastName: '',
+    company: 'Artforum',
+    title: ''
   }
 };
 
@@ -16,6 +24,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = state;
   }
 
@@ -26,9 +36,25 @@ class App extends Component {
     });
   }
 
-  handleChange(){}
+  handleChange(e){
+    let propName = e.target.name;
+    let propValue = e.target.value;
+    let replacer = {};
+    replacer[propName] = propValue;
+    let newCustomer = Object.assign({}, state.newCustomer, replacer);
+    let newState = update( this.state, {newCustomer: {$set: newCustomer}} );
+    console.log(newState);
+    this.setState(newState);
+    //this.setState({ newCustomer: newCustomer }); // W T F ????
+    console.log(state.newCustomer);
+  }
 
-  handleSubmit(){}
+  handleSubmit(e){
+    e.preventDefault();
+    this.setState((state)=> {
+      return state.editing = !state.editing;
+    })
+  }
 
   render() {
     const person = (
--- src/Form.js
+++ src/Form.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Form = (props) => {
   return (
-    <form>
-      The form
+    <form onSubmit={props.onSubmitCallback}>
+      <label>First Name</label>
+      <input onChange={props.onChangeCallback} name="firstName" type="text" defaultValue={props.data.firstName} />
+      <label>Last Name</label>
+      <input onChange={props.onChangeCallback} name="lastName" type="text" defaultValue={props.data.lastName} />
+      <label>Title</label>
+      <input onChange={props.onChangeCallback} name="title" type="text" defaultValue={props.data.title} />
+      <label>Company</label>
+      <input onChange={props.onChangeCallback} name="company" type="text" defaultValue={props.data.company} />
+      <button>Submit</button>
     </form>
   );
 };
